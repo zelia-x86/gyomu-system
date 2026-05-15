88371f92ef2c87c660703497f6382b92e323af90
zelia-x86 <209870153+zelia-x86@users.noreply.github.com>
2026-05-15 12:31:43 +0900

    set connections

--- src/main/java/com/gyomu/system/Main.java
+++ src/main/java/com/gyomu/system/Main.java
@@ -2,6 +2,8 @@ package com.gyomu.system;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Database db = new Database();
+        Window window = new Window(db);
+        window.setVisible(true);
     }
 }
