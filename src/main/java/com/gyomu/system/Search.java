fc5fa9e373c38a2443ebb60a94a0c77a0f5b4fe4
TY061026 <Tatuki1026tennis@icloud.com>
2026-05-20 10:37:30 +0900

    Searchを追加

--- src/main/java/com/gyomu/system/Search.java
+++ src/main/java/com/gyomu/system/Search.java
@@ -1,4 +1,6 @@
 package com.gyomu.system;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -15,6 +17,23 @@ public class Search extends JPanel {
         add(searchButton);
     }
     public String getSearchText() {
-        return searchField.getText();
+        
+        String targetText = "AA";
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == searchButton) {
+                    System.out.println("検索ボタンがクリックされました。");
+                }
+            }
+        });
+        String searchText = searchField.getText();
+        if (searchText != null && !searchText.isEmpty()) {
+            if (targetText.contains(searchText)) {
+                return searchText;
+            }
+        }
+        return null;
     }
-}
+    }
+
