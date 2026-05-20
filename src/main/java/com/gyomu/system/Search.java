51ddfd4e648fc6bc4e36b8490a449741e102fb13
zelia-x86 <209870153+zelia-x86@users.noreply.github.com>
2026-05-20 12:22:04 +0900

    Search code cleanup

--- src/main/java/com/gyomu/system/Search.java
+++ src/main/java/com/gyomu/system/Search.java
@@ -1,6 +1,4 @@
 package com.gyomu.system;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -17,25 +15,5 @@ public class Search extends JPanel {
         add(searchField);
         add(searchButton);
     }
-    public String getSearchText() {
-        
-        String targetText = "AA";
-        searchButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == searchButton) {
-                    System.out.println("検索ボタンがクリックされました。");
-                }
-            }
-        });
-        String searchText = searchField.getText();
-        if (searchText != null && !searchText.isEmpty()) {
-            if (targetText.contains(searchText)) {
-                System.out.println(searchText);
-                return searchText;
-            }
-        }
-        return null;
-    }
-    }
+}
 
